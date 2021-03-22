7805914e6e24885bffbcc9c7d0c60a3ca0f0cf5b
Icaro Oliveira <icarovinici@gmail.com>
2021-03-22 12:26:30 -0300

    feat: add sass + global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Head from 'next/head'
+
 export default function Home() {
-  return <h1>Hello world</h1>
+  return (
+    <>
+      <Head>
+        <title>Início | IG.NEWS</title>
+      </Head>
+      <h1>Hello world</h1>
+    </>
+  )
 }
