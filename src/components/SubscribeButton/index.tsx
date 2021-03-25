89c990458dc338977d5a089ed87b30a580eadfa1
Icaro Oliveira <icarovinici@gmail.com>
2021-03-25 11:43:14 -0300

    feat: add subscribeButton component

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss'
+
+export default function SubscribeButton() {
+  return (
+    <button type="button" className={styles.subscribeButton}>Subscribe Now</button>
+  )
+}
